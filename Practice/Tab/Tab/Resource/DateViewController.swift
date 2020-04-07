195e9cc901e11b5c919e2c0c564a48ee4811df9c
jana-choi <lovej0918@gmail.com>
2020-04-07 17:41:58 +0900

    10. Tab

---
+++ Practice/Tab/Tab/Resource/DateViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by 박재선 on 2020/04/05.
+//  Copyright © 2020 Jana. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+    let timeSelector: Selector = #selector(DateViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEEE"
+        lblPickerTime.text = formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+        //lblCurrentTime.text = String(count)
+        //count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
+        lblCurrentTime.text = formatter.string(from: date as Date)
+    }
+}
+
---
+++ Practice/Tab/Tab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by 박재선 on 2020/04/07.
+//  Copyright © 2020 Jana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
