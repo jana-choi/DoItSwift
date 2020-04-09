ea93cfd88305f9077b8735f1b47f6a7951c09677
jana-choi <lovej0918@gmail.com>
2020-04-09 11:48:52 +0900

    Auto Layout

---
+++ Practice/AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by 박재선 on 2020/04/09.
+//  Copyright © 2020 Jana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
